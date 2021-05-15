59d2c0d5c81cc256a6c0ea249765ec3bc4ef4b70
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2021-05-15 09:25:37 +0700

    change gcn to gat model

--- model/model.py
+++ model/model.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
-import torch_geometric.transforms as T
-from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
+from torch_geometric.nn import GCNConv, GATConv
 from ogb.graphproppred.mol_encoder import AtomEncoder
 from torch_geometric.nn import global_add_pool, global_mean_pool
 from torch_geometric.data import Data, Batch
@@ -51,6 +49,48 @@ class GCN(torch.nn.Module):
             return out
 
 
+class GAT(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers,
+                 dropout, return_embeds=False):
+        super(GAT, self).__init__()
+
+        gcn_conv_first = GATConv(in_channels=input_dim, out_channels=hidden_dim)
+        gcn_conv_last = GATConv(in_channels=hidden_dim, out_channels=output_dim)
+        gcn_conv_mid = GATConv(in_channels=hidden_dim, out_channels=hidden_dim)
+        self.convs = torch.nn.ModuleList([gcn_conv_first]
+                                         + [gcn_conv_mid for _ in range(num_layers-2)] + [gcn_conv_last])
+        self.bns = torch.nn.ModuleList([torch.nn.BatchNorm1d(num_features=hidden_dim) for _ in range(num_layers - 1)])
+        self.softmax = torch.nn.LogSoftmax(dim=1)
+        self.dropout = dropout
+
+        # Skip classification layer and return node embeddings
+        self.return_embeds = return_embeds
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, edge_index):
+        out = self.convs[0](x, edge_index)
+        out = self.bns[0](out)
+        out = F.relu(out, inplace=False)
+        out = F.dropout(out, self.dropout)
+        for layer in range(1, len(self.convs) - 1, 1):
+            out = self.convs[layer](out, edge_index)
+            out = self.bns[layer](out)
+            out = F.relu(out, inplace=False)
+            out = F.dropout(out, self.dropout)
+
+        out = self.convs[-1](out, edge_index)
+        if self.return_embeds:
+            return out
+        else:
+            out = self.softmax(out)
+            return out
+
+
 class GCNGraph(torch.nn.Module):
     def __init__(self, hidden_dim, output_dim, num_layers, dropout):
         super(GCNGraph, self).__init__()
@@ -58,7 +98,7 @@ class GCNGraph(torch.nn.Module):
         # Load encoders for Atoms in molecule graphs
         self.node_encoder = AtomEncoder(hidden_dim)
 
-        self.gnn_node = GCN(hidden_dim, hidden_dim, output_dim, num_layers, dropout, return_embeds=True)
+        self.gnn_node = GAT(hidden_dim, hidden_dim, output_dim, num_layers, dropout, return_embeds=True)
 
     def reset_parameters(self):
         self.gnn_node.reset_parameters()
@@ -94,7 +134,7 @@ class Conv1DBlock(torch.nn.Module):
 
 
 class MyModel(torch.nn.Module):
-    def __init__(self, drug_input_dim, dis_input_dim, hidden_dim, drug_output_dim,dis_output_dim,
+    def __init__(self, drug_input_dim, dis_input_dim, hidden_dim, drug_output_dim, dis_output_dim,
                  num_layers, num_layer2, dropout):
         super(MyModel, self).__init__()
 
