ca27610771a8b02dfb6aaa2597364ae9b1cfb238
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2021-05-16 12:45:13 +0700

    delete log

--- train.py
+++ train.py
@@ -38,20 +38,10 @@ class Trainer:
         self.drug_molecules = DrugDataset(drugs_file=drugs_file, device=self.device)
 
         # Build Model, Optimizer, Loss Function
-        self.model = MyModel(drug_input_dim=self.drug_graph.num_node_features,
-                             dis_input_dim=self.dis_graph.num_node_features, hidden_dim=256,
-                             drug_output_dim=128, dis_output_dim=128, num_layers=4,
-                             num_layer2=5, dropout=self.dropout)
-        self.model = self.model.to(self.device)
-
         self.loss_fn = WeightedFocalLoss(alpha=0.75)
 
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=20, gamma=0.5)
-
-    def train_one_epoch(self, dataloader):
-        self.model.train()
+    def train_one_epoch(self, dataloader, model, optimizer, scheduler):
+        model.train()
         loss_train = 0
 
         for step, batch in enumerate(dataloader):
@@ -60,21 +50,21 @@ class Trainer:
             label = batch['label']
             label = label.to(torch.float)
 
-            self.optimizer.zero_grad()
-            logit = self.model(self.drug_graph, drug_idx, self.dis_graph, dis_idx, self.drug_molecules[drug_idx])
+            optimizer.zero_grad()
+            logit = model(self.drug_graph, drug_idx, self.dis_graph, dis_idx, self.drug_molecules[drug_idx])
             logit = torch.squeeze(logit, dim=1)
             loss = self.loss_fn(logit, label)
             loss_train += loss.item()
 
             loss.backward()
-            self.optimizer.step()
+            optimizer.step()
 
-        self.scheduler.step()
+        scheduler.step()
 
         return loss_train
 
-    def eval_one_epoch(self, dataloader, threshold=0.5):
-        self.model.eval()
+    def eval_one_epoch(self, dataloader, model, threshold=0.5):
+        model.eval()
         total_loss = 0
         y_label = list()
         y_pred = list()
@@ -86,7 +76,7 @@ class Trainer:
             y_label.append(label.detach().cpu())
 
             with torch.no_grad():
-                logit = self.model(self.drug_graph, drug_idx, self.dis_graph, dis_idx, self.drug_molecules[drug_idx])
+                logit = model(self.drug_graph, drug_idx, self.dis_graph, dis_idx, self.drug_molecules[drug_idx])
                 logit = torch.squeeze(logit, dim=1)
                 pred = torch.sigmoid(logit)
                 y_pred.append(pred.detach().cpu())
@@ -115,16 +105,26 @@ class Trainer:
 
             train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
             test_dataloader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=True)
+
+            model = MyModel(drug_input_dim=self.drug_graph.num_node_features,
+                                 dis_input_dim=self.dis_graph.num_node_features, hidden_dim=256,
+                                 drug_output_dim=128, dis_output_dim=128, num_layers=4,
+                                 num_layer2=5, dropout=self.dropout)
+            model = model.to(self.device)
+
+            optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+            scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+
             file_log = base_root + '{}th.txt'.format(k + 1)
 
             for epoch in tqdm(range(self.n_epoch)):
                 print("============Epoch {}============".format(epoch + 1))
                 print("Training epoch {}".format(epoch + 1))
-                loss = self.train_one_epoch(train_dataloader)
+                loss = self.train_one_epoch(train_dataloader, model, optimizer, scheduler)
 
                 print("Evaluating epoch {}".format(epoch + 1))
-                train_result = self.eval_one_epoch(train_dataloader)
-                test_result = self.eval_one_epoch(test_dataloader)
+                train_result = self.eval_one_epoch(train_dataloader, model)
+                test_result = self.eval_one_epoch(test_dataloader, model)
 
                 train_loss, train_auc, train_ap = train_result
                 test_loss, test_auc, test_ap = test_result
@@ -134,7 +134,7 @@ class Trainer:
                       .format(train_loss, train_auc, train_ap, test_auc, test_ap))
 
             print("Evaluating on test dataset")
-            test_result = self.eval_one_epoch(test_dataloader)
+            test_result = self.eval_one_epoch(test_dataloader, model)
             _, test_auc, test_ap = test_result
             avg_ap += test_ap
             avg_auc += test_auc
@@ -146,4 +146,4 @@ class Trainer:
 if __name__ == '__main__':
     trainer = Trainer('./data/drug_dis.csv', './data/drug_sim.csv', './data/dis_sim.csv', './data/drugs.csv',
                       lr=0.01, n_epoch=40, dropout=0.2, batch_size=128)
-    trainer.train(base_root='./weight/5_fold/')
+    trainer.train(base_root='./weight/5_fold_v2/')
